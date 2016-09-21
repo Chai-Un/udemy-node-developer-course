7f473bc5886081f3696c93c0aa5f30425892042a
MazeFX <filoplast@gmail.com>
2016-09-21 07:23:55 +0200

    Starting bank account video

---
+++ bank-account.js
@@ -0,0 +1,3 @@
+/**
+ * Created by MazeFX on 21-9-2016.
+ */
--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
-var person = 
+var pet = {
+    name: 'Balou',
+    type: 'cat'
+};
+
+function printPet (pet) {
+    console.log('You own a ' + pet.type + ' and its name is ' + pet.name);
+}
+
+printPet(pet);
