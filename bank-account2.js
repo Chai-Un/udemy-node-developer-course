192b66ce4b1a245c4d97d646e95e5d2a684a7aca
MazeFX <filoplast@gmail.com>
2016-09-22 12:20:39 +0200

    Bank account2 video completed.

--- bank-account2.js
+++ bank-account2.js
@@ -32,16 +32,6 @@ function getBalance (account) {
     return account.balance;
 }
 
-console.log('Your balance is now: ' + account.balance);
-
-deposit(account, 100);
-console.log('Your balance is now: ' + account.balance);
-
-withdraw(account, 20);
-console.log('Your balance is now: ' + account.balance);
-
-console.log('Getting balance using function, balance = ' + getBalance(account));
-
 createAccount({
     balance: 12,
     username: 'Nick'
