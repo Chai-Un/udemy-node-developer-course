9f2a1d278d6c632d0f25472c88b11b8879d164f4
MazeFX <filoplast@gmail.com>
2016-09-26 12:54:57 +0200

    Password manager save variables local succesfull: vid 26 completed

--- password-manager/app.js
+++ password-manager/app.js
@@ -3,4 +3,48 @@ console.log('Starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
+// account.name Facebook
+// account.username User12!
+// account.password Password123! 
 
+function createAccount(account) {
+	var accounts = storage.getItemSync('accounts');
+
+	// if accounts is undefined (use typeof)
+	// 		set accounts = []
+	if (typeof accounts === 'undefined') {
+		accounts = [];
+	}
+
+	// push on new account
+	accounts.push(account);
+	storage.setItemSync('accounts', accounts);
+
+	// return account
+	return account;
+}
+
+function getAccount(accountName) {
+	// var accounts use getitemSync(accounts)
+	var accounts = storage.getItemSync('accounts');
+	var matchedAccount;
+
+	if (typeof accounts !== 'undefined') {
+		for (var i=0; i < accounts.length; i++) {
+			if (accounts[i].name === accountName) {
+				matchedAccount = accounts[i];
+			}
+		}
+	}
+	return matchedAccount;
+
+	// iterate over array, return matching account, else undefined
+}
+
+// createAccount({
+// 	name: 'Facebook',
+// 	username: 'User12!',
+// 	password: 'Password123!'
+// })
+
+console.log(getAccount('Facebook'));
