b864d315bca803cd26c47e2f468c8de101645bf3
MazeFX <filoplast@gmail.com>
2016-09-26 07:25:58 +0200

    password manager video started: vid24

--- strings.js
+++
@@ -1,7 +0,0 @@
-var name = 'Nick';
-var lastName = 'Geense';
-
-var fullName = name + ' ' + lastName;
-
-console.log(fullName);
-console.log(fullName.length);
--- variables.js
+++
@@ -1,6 +0,0 @@
-var name = 'Nick';
-
-var hello = 'Hello Nick!';
-
-console.log(name);
-console.log(hello);
