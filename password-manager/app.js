b83a5c2d8356107ebf8c1baa6e4262abe432c78c
MazeFX <filoplast@gmail.com>
2016-09-26 12:26:18 +0200

    start building password manager: vid 26 started

--- password-manager/app.js
+++ password-manager/app.js
@@ -3,18 +3,4 @@ console.log('Starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
-// storage.setItemSync('account', [{
-// 	username: 'nick',
-// 	balance: '0'
-// }]);
-var account = storage.getItemSync('account');
-console.log(account);
 
-// account.push({
-// 	username: 'Stefanie',
-// 	balance: 10000
-// })
-
-// console.log(account);
-
-// storage.setItemSync('account', account);
