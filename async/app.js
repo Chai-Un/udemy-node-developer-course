65787a63918a2828571f02f3d463a3c2587366a8
MazeFX <filoplast@gmail.com>
2016-10-03 12:52:22 +0200

    That's the flute, gotta go back to work.

--- async/app.js
+++ async/app.js
@@ -1,16 +1,35 @@
 var weather = require('./weather.js');
 var location = require('./location.js');
 
-weather(function(currentWeather) {
-	console.log(currentWeather);
-});
+// setup yargs for -location or -l
+var argv = require('yargs')
+	.option('location', {
+		demand: false,
+		alias: 'l',
+		description: 'Location name goes here',
+		type: 'string'
+	})
+	.help('help');
+	.argv;
 
-location(function (location) {
-	if (!location) {
-		console.log('Unable to get location');
-		return;
-	} else {
-		console.log('city: ' + location.city);
-		console.log('long/lat: ' + location.loc);
-	}
-});
+// location(function (location) {
+// 	if (!location) {
+// 		console.log('Unable to get location');
+// 		return;
+// 	} else {
+// 		console.log('city: ' + location.city);
+// 		console.log('long/lat: ' + location.loc);
+// 	}
+// });
+
+// if location is provided 
+//  call weather
+
+if (typeof argv.l === 'string' && argv.l.length > 0) {
+	console.log('location provided = ' + argv.l);
+	weather(argv.l, function(currentWeather) {
+		console.log(currentWeather);
+	});
+} else {
+	console.log('location not provided! Trying to gues where you are..');
+}
--- async/weather.js
+++ async/weather.js
@@ -1,11 +1,15 @@
 var request = require('request');
 
 var apiKey = 'e2cac73b15943c192519aba540aee144';
-var encodedLocation = 'Rotterdam';
-var url = ('http://api.openweathermap.org/data/2.5/weather?appid=' + apiKey + 
+
+module.exports = function(location, callback) {
+	var encodedLocation = encodeURIComponent(location);
+	if (!encodedLocation) {
+		return callback('No location for fetching weather');
+	}
+	var url = ('http://api.openweathermap.org/data/2.5/weather?appid=' + apiKey + 
 	'&q=' + encodedLocation + '&units=metric');
 
-module.exports = function(callback) {
 	request({url: url, json: true}, function(error, response, body) {
 		if (error) {
 			callback('Unable to fetch weather');
