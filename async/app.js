2bd3006f0ccd725d01c628dfe0dd657f82f69680
MazeFX <filoplast@gmail.com>
2016-10-03 07:37:43 +0200

    location gues function created: vid 40 completed

--- async/app.js
+++ async/app.js
@@ -1,7 +1,16 @@
 var weather = require('./weather.js');
-
-
+var location = require('./location.js');
 
 weather(function(currentWeather) {
 	console.log(currentWeather);
+});
+
+location(function (location) {
+	if (!location) {
+		console.log('Unable to get location');
+		return;
+	} else {
+		console.log('city: ' + location.city);
+		console.log('long/lat: ' + location.loc);
+	}
 });
