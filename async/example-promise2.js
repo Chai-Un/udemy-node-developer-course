4c1a3ed3ebec8808428b192503c58065433995c4
MazeFX <filoplast@gmail.com>
2016-10-10 12:13:01 +0200

    Promises explained: vid 43 completed

--- async/example-promise2.js
+++ async/example-promise2.js
@@ -1,16 +1,34 @@
-function doWork() {
-	return new Promise(function(resolve, reject) {
-		setTimeout(function() {
-			console.log('done!');
-			resolve();
-		}, 1000);
+// function doWork() {
+// 	return new Promise(function(resolve, reject) {
+// 		setTimeout(function() {
+// 			console.log('done!');
+// 			resolve();
+// 		}, 1000);
+// 	});
+// }
+
+// doWork().then(function() {
+// 	return doWork();
+// }).then(function() {
+// 	console.log('all done!');
+// }, function() {
+	
+// });
+
+function getLocation() {
+	return new Promise(function (resolve, reject) {
+		resolve('Philadelphia');
 	});
 }
 
-doWork().then(function() {
-	return doWork();
-}).then(function() {
-	console.log('all done!');
-}, function() {
-	
+function getWeather(location) {
+	return new Promise(function (resolve, reject) {
+		resolve('It is 78 in ' + location + '!');
+	});
+}
+
+getLocation().then(function (location) {
+	return getWeather(location);
+}).then(function(currentWeather) {
+	console.log(currentWeather);
 });
